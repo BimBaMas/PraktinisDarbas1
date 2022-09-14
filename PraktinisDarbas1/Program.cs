9321cb0cc58b4aa5e7083426da7a6f2c62d41327
BimBaMas <dagilis@gmail.com>
2022-09-14 19:29:41 +0300

    Padarytas isvadimas i konsole.

--- PraktinisDarbas1/Program.cs
+++ PraktinisDarbas1/Program.cs
@@ -1,19 +1,25 @@
-﻿/*
-Susklaiciuoti zemes rutulio plota. 6371km S = 4 π * R^2
- */
+﻿//Susklaiciuoti zemes rutulio plota. 6371km S = 4 π * R^2
 
 double plotas = 4 * 3.14 * 6371 * 6371;
 Console.WriteLine($"Plotas : {plotas} kv. km");
 
+/*apskaičiuoti žemės rutulio tūrį*/
+
+double volume = 4 / 3 * 3.14 * 6371 * 6371 * 6371;
+Console.WriteLine($"tūris: {volume}");
 
 //3a uzduotis
 
 int statinis1 = 10;
 int statinis2 = 15;
+double izambine = Math.Sqrt(Math.Pow(statinis1, 2) + Math.Pow(statinis2, 2));
 
-double izambine = Math.Pow(statinis1, 2) + Math.Pow(statinis2, 2);
-Console.WriteLine($"Hello, World! {izambine}");
-/*apskaičiuoti žemės rutulio tūrį*/
-
-double volume = 4 / 3 * 3.14 * 6371 * 6371 * 6371;
-Console.WriteLine($"tūris: {volume}");
+Console.WriteLine("**********************************");
+Console.WriteLine("Viso kintamuju : 3");
+Console.WriteLine($"Kintamasis1 : {plotas}");
+Console.WriteLine($"Kintamasis2 : {izambine}");
+Console.WriteLine($"Kintamasis3 : {volume}");
+double suma = plotas + izambine + volume;
+Console.WriteLine($"Suma : {suma}");
+Console.WriteLine($"Vidurkis : {suma / 3}");
+Console.WriteLine("**********************************");

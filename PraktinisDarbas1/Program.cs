e9e71b1f2869bf1676a1e410c9a4196123c0a7eb
BimBaMas <dagilis@gmail.com>
2022-09-14 18:49:04 +0300

    Suskaiciuotas plotas

--- PraktinisDarbas1/Program.cs
+++ PraktinisDarbas1/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+/*
+Susklaiciuoti zemes rutulio plota. 6371km S = 4 π * R^2
+ */
+
+double plotas = 4 * 3.14 * 6371 * 6371;
+Console.WriteLine($"Plotas : {plotas} kv. km");

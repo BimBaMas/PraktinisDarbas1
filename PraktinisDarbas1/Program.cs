03924a9f4be503f3188c88393acd6109a82c5e03
ryrytis <106332962+ryrytis@users.noreply.github.com>
2022-09-14 19:00:08 +0300

    3rdexercise

--- PraktinisDarbas1/Program.cs
+++ PraktinisDarbas1/Program.cs
@@ -6,3 +6,12 @@ Susklaiciuoti zemes rutulio plota. 6371km S = 4 π * R^2
 
 double plotas = 4 * 3.14 * 6371 * 6371;
 Console.WriteLine($"Plotas : {plotas} kv. km");
+
+
+//3a uzduotis
+
+int statinis1 = 10;
+int statinis2 = 15;
+
+double izambine = Math.Pow(statinis1, 2) + Math.Pow(statinis2, 2);
+Console.WriteLine($"Hello, World! {izambine}");

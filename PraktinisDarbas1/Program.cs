8cfe92aeb294e9572b277d1fcaddd378c1cb0314
ryrytis <106332962+ryrytis@users.noreply.github.com>
2022-09-14 18:44:40 +0300

    3rd exercise

--- PraktinisDarbas1/Program.cs
+++ PraktinisDarbas1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int statinis1 = 10;
+int statinis2 = 15;
+
+double izambine = Math.Pow(statinis1, 2) + Math.Pow(statinis2, 2);
+Console.WriteLine($"Hello, World! {izambine}");
